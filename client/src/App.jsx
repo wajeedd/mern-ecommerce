e5298c2f4c4d9806b199229d621928d1a5a90af7
wajeedd <238w1a12i2@vrsec.ac.in>
2026-04-16 10:08:56 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BrowserRouter,Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Register from './components/Register'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/register' element={<Register/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,66 @@
+import React,{useState} from 'react'
+import API from '../api/axios'
+export default function Register() {
+    const [name,setName]=useState("")
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const [mobile,setMobile]=useState("")
+    function handleRegister(e){
+        e.preventDefault()
+        API.post("/auth/register",{name,email,password,mobile:Number(mobile)})
+            .then((res)=>{
+                console.log(res)
+                if(res.status===201){
+                    alert("User created successfully")
+                }
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    }
+    return (
+        <div className='container'>
+            <div className='row'>
+                <form onSubmit={handleRegister} className='col-12 col-md-6'>
+                    <div className='mb-3'>
+                        <h1>Register</h1>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="" class="form-label">Name</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            name="name"
+                            onChange={(e)=>setName(e.target.value)}/>
+                    </div>
+                    <div class="mb-3">
+                        <label htmlFor="" class="form-label">Email</label>
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            name="email"
+                            onChange={(e)=>setEmail(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="" class="form-label">Password</label>
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            name="password"
+                            onChange={(e)=>setPassword(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="" className="form-label">Mobile Number</label>
+                        <input 
+                            type="text" 
+                            class="form-control" 
+                            name="mobile"
+                            onChange={(e)=>setMobile(e.target.value)}/>
+                    </div>
+                    <button className='btn btn-success'>Register</button>
+                </form>
+            </div>
+
+        </div>
+    )
+}
