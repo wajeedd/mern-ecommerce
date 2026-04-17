bbb4d03cf9d3634304b2dbdaf5ffa33571b46abe
wajeedd <238w1a12i2@vrsec.ac.in>
2026-04-17 15:18:50 +0530

    Day2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Login from './components/Login'
 import Addproduct from './components/Addproduct'
 import Home from './components/Home'
 import ProtectedRoute from './components/ProtectedRoute'
+import Cart from './components/Cart'
+
 
 export default function App() {
   return (
@@ -18,6 +20,11 @@ export default function App() {
         <Route path="/add-product" element={
           <ProtectedRoute>
             <Addproduct/>
+          </ProtectedRoute>
+        }/>
+        <Route path="/cart" element={
+          <ProtectedRoute>
+            <Cart/>
           </ProtectedRoute>
         }/>
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import API from "../api/axios";
-
+import {useNavigate} from 'react-router-dom'
 function Home() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
- const role=localStorage.getItem("user.role")
+  const navigate=useNavigate()
+  const role=localStorage.getItem("role")
   useEffect(() => {
     fetchProducts();
   }, []);
@@ -22,7 +23,16 @@ function Home() {
   };
 
   const addToCart = async (id) => {
-    
+    API.post("/cart/add",{productId:id})
+      .then((res)=>{
+        if(res.status==201){
+          alert("Added to cart")
+          navigate("/cart")
+        }
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
   };
 
   return (
