e4b94aed0bbd915aeae6419d9fdd345237f90078
wajeedd <238w1a12i2@vrsec.ac.in>
2026-04-17 16:29:10 +0530

    Day2

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"http://https://mern-ecommerce-ebry.onrender.com/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
